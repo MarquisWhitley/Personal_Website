01a06270f7d06be1269aa4ac7601e128fdb66631
Marquis Whitley <whitley.marquis1@yahoo.com>
2020-11-23 16:45:44 -0500

    added google analytics

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-const rightArrow = document.querySelector('.right-arrow');
-const leftArrow = document.querySelector('.left-arrow');
 const projects = document.querySelector('.section2');
 const more = document.querySelector('.section4');
 const aboutMe = document.querySelector('.section1');
 const heading = document.querySelector('#heading');
 const contact = document.querySelector('.contact');
-const leftArrow2 = document.querySelector('.left-arrow2');
-const rightArrow2 = document.querySelector('.right-arrow2');
-const rightArrow3 = document.querySelector('.right-arrow3');
+
+const projectsRightButton = document.querySelector('.right-arrow');
+const aboutMeLeftButton = document.querySelector('.left-arrow');
+const contactLeftButton = document.querySelector('.left-arrow2');
+const aboutMeRightButton = document.querySelector('.right-arrow2');
+const moreProjectsRightButton = document.querySelector('.right-arrow3');
+
 const submit = document.querySelector('.form-submitted');
 const message = document.querySelector('.contact_message');
 
 const form = document.querySelector('.contact_form');
 form.addEventListener('submit', (e) => submitForm(e));
 
-rightArrow.addEventListener('click', () => arrowRight());
-rightArrow2.addEventListener('click', () => arrowRight2());
-rightArrow3.addEventListener('click', () => arrowRight3());
-leftArrow.addEventListener('click', () => arrowLeft());
-leftArrow2.addEventListener('click', () => arrowLeft2());
+projectsRightButton.addEventListener('click', () => projectsRight());
+aboutMeRightButton.addEventListener('click', () => aboutMeRight());
+moreProjectsRightButton.addEventListener('click', () => moreProjectsRight());
+aboutMeLeftButton.addEventListener('click', () => aboutMeLeft());
+contactLeftButton.addEventListener('click', () => contactLeft());
+
+const delay = async (delay) =>
+  await new Promise((resolve, reject) => setTimeout(resolve, delay));
 
 let currentHeading = 40;
 let currentAboutMeL = 28;
@@ -27,73 +32,65 @@ if (document.body.clientWidth < 500) {
   currentHeading = 28;
   currentAboutMeL = 19;
 }
+const hideButtons = (...args) => {
+  args.forEach((vals) => (vals.style.visibility = 'hidden'));
+};
+const showButtons = (classes, ...args) => {
+  args.forEach((vals) => {
+    vals.style.visibility = 'visible';
+    vals.classList.remove(classes);
+  });
+};
+const addClass = (classes, ...args) => {
+  args.forEach((vals) => vals.classList.add(classes));
+};
 
-const arrowRight = async () => {
-  rightArrow.classList.add('clicked');
-  leftArrow2.classList.add('clicked');
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  rightArrow.style.visibility = 'hidden';
-  leftArrow2.style.visibility = 'hidden';
+const projectsRight = async () => {
+  addClass('clicked', projectsRightButton, contactLeftButton);
+  await delay(500);
+  hideButtons(projectsRightButton, contactLeftButton);
   projects.style.right = '19%';
   aboutMe.style.left = '-100%';
   heading.style.left = '-100%';
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  leftArrow.classList.remove('clicked');
-  leftArrow.style.visibility = 'visible';
-  rightArrow3.style.visibility = 'visible';
-  rightArrow3.classList.remove('clicked');
+  await delay(500);
+  showButtons('clicked', aboutMeLeftButton, moreProjectsRightButton);
 };
-
-const arrowRight2 = async () => {
-  rightArrow2.classList.add('clicked');
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  rightArrow2.style.visibility = 'hidden';
+const moreProjectsRight = async () => {
+  addClass('clicked', moreProjectsRightButton, aboutMeLeftButton);
+  await delay(500);
+  hideButtons(aboutMeLeftButton, moreProjectsRightButton);
+  projects.style.right = '200%';
+  more.style.right = '20%';
+  await delay(500);
+  showButtons('clicked', aboutMeLeftButton);
+};
+const aboutMeRight = async () => {
+  addClass('clicked', aboutMeRightButton);
+  await delay(500);
+  hideButtons(aboutMeRightButton);
   contact.style.left = '-100%';
   aboutMe.style.left = `${currentAboutMeL}%`;
   heading.style.left = `${currentHeading}%`;
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  rightArrow.classList.remove('clicked');
-  rightArrow.style.visibility = 'visible';
-  leftArrow2.style.visibility = 'visible';
-  leftArrow2.classList.remove('clicked');
+  await delay(500);
+  showButtons('clicked', projectsRightButton, contactLeftButton);
 };
 
-const arrowRight3 = async () => {
-  rightArrow3.classList.add('clicked');
-  leftArrow.classList.add('clicked');
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  leftArrow.style.visibility = 'hidden';
-  rightArrow3.style.visibility = 'hidden';
-  projects.style.right = '200%';
-  more.style.right = '20%';
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  leftArrow.classList.remove('clicked');
-  leftArrow.style.visibility = 'visible';
-};
-
-const arrowLeft = async () => {
-  leftArrow.classList.add('clicked');
-  rightArrow3.classList.add('clicked');
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  leftArrow.style.visibility = 'hidden';
-  rightArrow3.style.visibility = 'hidden';
+const aboutMeLeft = async () => {
+  addClass('clicked', aboutMeLeftButton, moreProjectsRightButton);
+  await delay(500);
+  hideButtons(aboutMeLeftButton, moreProjectsRightButton);
   projects.style.right = '-100%';
   more.style.right = '-200%';
   aboutMe.style.left = `${currentAboutMeL}%`;
   heading.style.left = `${currentHeading}%`;
-  await new Promise((resolve, reject) => setTimeout(resolve, 900));
-  leftArrow2.classList.remove('clicked');
-  leftArrow2.style.visibility = 'visible';
-  rightArrow.style.visibility = 'visible';
-  rightArrow.classList.remove('clicked');
+  await delay(900);
+  showButtons('clicked', contactLeftButton, projectsRightButton);
 };
 
-const arrowLeft2 = async () => {
-  rightArrow.classList.add('clicked');
-  leftArrow2.classList.add('clicked');
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  rightArrow.style.visibility = 'hidden';
-  leftArrow2.style.visibility = 'hidden';
+const contactLeft = async () => {
+  addClass('clicked', projectsRightButton, contactLeftButton);
+  await delay(500);
+  hideButtons(projectsRightButton, contactLeftButton);
   if (document.body.clientWidth < 600) {
     contact.style.left = '10%';
   } else {
@@ -101,9 +98,8 @@ const arrowLeft2 = async () => {
   }
   aboutMe.style.left = '200%';
   heading.style.left = '200%';
-  await new Promise((resolve, reject) => setTimeout(resolve, 500));
-  rightArrow2.classList.remove('clicked');
-  rightArrow2.style.visibility = 'visible';
+  await delay(500);
+  showButtons('clicked', aboutMeRightButton);
 };
 
 const submitForm = async (e) => {
@@ -125,3 +121,37 @@ const submitForm = async (e) => {
     return;
   }
 };
+
+// const scrollableElement = document.body;
+// let position = 0;
+
+// const checkScrollDirection = (event) => {
+//   if (checkScrollDirectionIsUp(event)) {
+//     console.log('UP');
+//     position--;
+//   }
+//   if (!checkScrollDirectionIsUp(event)) {
+//     console.log('Down');
+//     position++;
+//   }
+//   if (position >= 3) {
+//     return;
+//   }
+//   if (position <= -1) {
+//     return;
+//   }
+//   console.log(position);
+// };
+
+// const checkPos = () => {
+//   // if(position = 1) console.log(position)
+//   if ((position = 2)) console.log(`this is position ${position}`);
+// };
+
+// const checkScrollDirectionIsUp = (event) => {
+//   if (event.wheelDelta) {
+//     return event.wheelDelta > 0;
+//   }
+//   return event.deltaY < 0;
+// };
+// scrollableElement.addEventListener('wheel', checkScrollDirection);
